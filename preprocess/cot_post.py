085089da1eea5d9486dacabc69c840cdbc8d4df0
lilinwz <lilin22wz@gmail.com>
2025-07-31 07:18:25 +0000

    daily update

--- preprocess/cot_post.py
+++ preprocess/cot_post.py
@@ -14,30 +14,29 @@ for item in data:
     cot2 = item['cot2']
     cot3 = item['cot3']
     
-    info_in = query
-    info_out = ""
-
     # if len(chain) in a:
     #     a[len(chain)] += 1
     # else:
     #     a[len(chain)] = 1
     if len(chain) == 1:
         continue
-    data_item = [{"role": "user", "content": info_in}]
+
+    data_item = {"user": query, "assistant": []}
+    info = ""
     for i, sub_item in enumerate(chain):
         subquery = sub_item['subquery']
         subanswer = sub_item['sub-answer']
         context = sub_item['paragraph']
         
-        info_out += f"<think>\n{cot1[i]}\n</think>\n<retrieval>\n{subquery}\n</retrieval>\n\n"
-        data_item.append({"role": "assistant", "content": info_out})
+        info += f"<think>\n{cot1[i]}\n</think>\n<subquery>\n{subquery}\n</subquery>\n<retrieval>"
+        data_item["assistant"].append(info)
 
-        info_in = context
-        data_item.append({"role": "user", "content": info_in})
-        info_out = f"<think>\n{cot2[i]}\n</think>\n<><subanswer>\n{subanswer}\n</subanswer>\n\n"
+        info = f"\n{context}\n</retrieval>\n"
+        data_item["assistant"].append(info)
+        info = f"<think>\n{cot2[i]}\n</think>\n<><subanswer>\n{subanswer}\n</subanswer>\n\n"
 
-    info_out += f"<think>\n{cot3}\n</think>\n<answer>\n{answer}\n</answer>"
-    data_item.append({"role": "assistant", "content": info_out})
+    info += f"<think>\n{cot3}\n</think>\n<answer>\n{answer}\n</answer>"
+    data_item["assistant"].append(info)
 
     new_data.append(data_item)
 
